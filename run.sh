86231bc4075395e5b55ac7443552e8e516bf7218
Michael Palotas <michael.palotas@element34.com>
2017-11-29 15:29:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mono /mono/selenium.2.53/bin/Debug/selenium.2.53.exe
-mono /mono/selenium.3.4.0/bin/Debug/selenium.3.4.0.exe
+#mono /mono/selenium.3.4.0/bin/Debug/selenium.3.4.0.exe
 mono /mono/selenium.3.5.2/bin/Debug/selenium.3.5.2.exe
+mono /mono/selenium.3.7.0/bin/Debug/selenium.3.7.0.exe
