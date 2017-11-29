f1486538a3545cece44545dac112207015122c95
Michael Palotas <michael.palotas@element34.com>
2017-11-29 10:17:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # A sample Bash script, by Ryan
+mono /mono/selenium.2.53.1/bin/Debug/selenium.2.53.1.exe
 mono /mono/selenium.3.4.0/bin/Debug/selenium.3.4.0.exe
 mono /mono/selenium.3.5.2/bin/Debug/selenium.3.5.2.exe
